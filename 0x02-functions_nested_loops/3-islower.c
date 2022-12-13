3761a8450cbe651c6771d258cbfb1e52b3722478
Oladejobola <bolaolad1@gmail.com>
2022-12-13 10:55:07 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Print alphabet in lowercase 10 times
- * Return 0
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
  */
 
-void print_alphabet_x10(void)
+int _islower(int c)
 {
-	char c;
-	int count;
-
-	for (count = 0; count < 10; count++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
-	}
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
